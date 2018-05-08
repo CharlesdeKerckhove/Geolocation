a88a395714a25b5e10bb6dbabfa3e8a697ebcc3b
CrazyC008 <DEKC1_16@uni.worc.ac.uk>
2018-05-08 06:37:37 +0100

    code

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ function getPosition() {
 //called when the position is successfully determined
 function successPosition(position) {
 	
-	//You can find out more details about what the position obejct contains here:
-	// http://www.w3schools.com/html/html5_geolocation.asp
-	
 
 	//lets get some stuff out of the position object
 	var time = position.timestamp;
@@ -39,6 +36,102 @@ function successPosition(position) {
 	$('#lattext').val(latitude);
 	$('#longtext').val(longitude);
 	
+    var Map;
+    var Infowindow;
+    // Get geo coordinates
+        getPlaces(latitude, longitude);
+
+    // Get places by using coordinates
+
+    function getPlaces(latitude, longitude) {
+
+        var latLong = new google.maps.LatLng(latitude, longitude);
+
+        var mapOptions = {
+
+            center: new google.maps.LatLng(latitude, longitude),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+
+        };
+
+        Map = new google.maps.Map(document.getElementById("places"), mapOptions);
+
+        Infowindow = new google.maps.InfoWindow();
+
+        var service = new google.maps.places.PlacesService(Map);
+        service.nearbySearch({
+
+            location: latLong,
+            radius: 500,
+            type: ['pub']
+        }, foundStoresCallback);
+
+    }
+
+    // Success callback for watching your changing position
+
+    var onPlacesWatchSuccess = function (position) {
+
+        var updatedlatitude = position.coords.latitude;
+        var updatedlongitude = position.coords.longitude;
+
+        if (updatedlatitude != latitude && updatedlongitude != longitude) {
+
+            latitude = updatedlatitude;
+            longitude = updatedlongitude;
+
+            getPlaces(updatedlatitude, updatedlongitude);
+        }
+    }
+
+    // Success callback for locating stores in the area
+
+    function foundStoresCallback(results, status) {
+
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+            for (var i = 0; i < results.length; i++) {
+
+                createMarker(results[i]);
+
+            }
+        }
+    }
+
+    // Place a pin for each store on the map
+
+    function createMarker(place) {
+
+        var placeLoc = place.geometry.location;
+
+        var marker = new google.maps.Marker({
+            map: Map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+
+            Infowindow.setContent(place.name);
+            Infowindow.open(Map, this);
+
+        })
+    }
+
+    // Error callback
+
+    function onPlacesError(error) {
+        console.log('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    // Watch your changing position
+
+    function watchPlacesPosition() {
+
+        return navigator.geolocation.watchPosition
+        (onPlacesWatchSuccess, onPlacesError, { enableHighAccuracy: true });
+    }
 }
 
 //called if the position is not obtained correctly
